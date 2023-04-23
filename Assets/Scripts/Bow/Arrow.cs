2ee353e9b529818d4fd01ba6818fa498bcf8658e
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2023-04-23 15:03:39 +0300

    Arrow GetStuck

--- Assets/Scripts/Bow/Arrow.cs
+++ Assets/Scripts/Bow/Arrow.cs
@@ -2,41 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Arrow : MonoBehaviour {
+public class Arrow : MonoBehaviour
+{
 
-   public Rigidbody rb;
-   public TrailRenderer trailRenderer;
+    public Rigidbody rb;
+    public TrailRenderer trailRenderer;
 
-
-    public void SetToRope(Transform ropeTransform , Transform bow) {
-       transform.parent=ropeTransform;
-       transform.localPosition=Vector3.zero;
-       transform.localRotation=Quaternion.identity;
-        
-       rb.isKinematic=true;
-       trailRenderer.enabled=false;
-    }
-
-    public void Shot(float velocity) {
-      transform.parent=null;
-      rb.isKinematic=false;
-      rb.velocity=transform.forward * velocity;
-      trailRenderer.Clear();
-      trailRenderer.enabled=true;
-      Destroy(gameObject , 5f);
-
-    }
-
-
-  void OnCollisionEnter(Collision other)
+    public void SetToRope(Transform ropeTransform, Transform bow)
     {
+        transform.parent = ropeTransform;
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
 
-       if(other.transform.tag=="shootable")
-       {
-          rb.velocity=Vector3.zero;
-          rb.isKinematic=true;
-       }
+        rb.isKinematic = true;
+        trailRenderer.enabled = false;
+    }
+
+    public void Shot(float velocity)
+    {
+        transform.parent = null;
+        rb.isKinematic = false;
+        rb.velocity = transform.forward * velocity;
+        trailRenderer.Clear();
+        trailRenderer.enabled = true;
+        Destroy(gameObject, 5f);
 
     }
 
+    private void GetStuck()
+    {
+        rb.velocity = Vector3.zero;
+        rb.isKinematic = true;
+    }
+
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag != "Player")
+        {
+            GetStuck();
+        }
+
+        if (other.transform.tag == "shootable")
+        {
+            GetStuck();
+        }
+    }
 }
