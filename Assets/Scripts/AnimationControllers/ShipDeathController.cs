b813467825abb73500f9e59b643a262a4c8d9ce2
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2023-04-28 15:06:03 +0300

    ShipDeathController

--- Assets/Scripts/AnimationControllers/ShipDeathController.cs
+++ Assets/Scripts/AnimationControllers/ShipDeathController.cs
@@ -14,14 +14,15 @@ public class ShipDeathController : MonoBehaviour
     //[SerializeField] [Range(0, 40)] private float _rotationMaxY = 40;
     [SerializeField] [Range(0, 40)] private float _rotationMaxZ = 40;
 
-    [SerializeField] private float _smooth;
+    [SerializeField] [Range(0, 10)] private float _smooth;
+    [SerializeField] [Range(0, 1)] private float _time = 0.4f;
 
     private float rotationX => Random.Range(_rotationMinX, _rotationMaxX);
     //private float rotationY => Random.Range(_rotationMinY, _rotationMaxY);
     private float rotationZ => Random.Range(_rotationMinZ, _rotationMaxZ);
 
     [SerializeField] private float _rotationX;
-    [SerializeField] private float _rotationY;
+    //[SerializeField] private float _rotationY;
     [SerializeField] private float _rotationZ;
 
     private void Start()
@@ -32,48 +33,20 @@ public class ShipDeathController : MonoBehaviour
 
     private IEnumerator Die()
     {
-        Quaternion target = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z + rotationZ);
+        _rotationX = rotationX;
+        _rotationZ = rotationZ;
 
-        while (true)
+        Vector3 rotation = new Vector3(_rotationX, 0, _rotationZ) * _smooth * Time.deltaTime;
+        Debug.Log(rotation);
+
+        while (_time > 0)
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * _smooth);
+            _time -= Time.deltaTime;
+
+            transform.Rotate(rotation);
+
             yield return null;
         }
-
-
-        //Vector3 vector3 = new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z) + new Vector3(_rotationX, 0, _rotationZ);
-        ////кватернион не 360 а 0-1
-        //Debug.Log("transform.rotation = " + transform.rotation);
-        //Debug.Log("transform.rotation.y = " + transform.rotation.y);
-        //Debug.Log("vector3.y = " + vector3.y);
-        //float y = transform.rotation.y;
-        //Quaternion target = Quaternion.Euler(vector3.x, y, vector3.z);
-        //Debug.Log("target.y = " + target.y);
-
-        //while (true)
-        //{
-        //    Debug.Log("transform.rotation = " + transform.rotation);
-        //    Debug.Log("transform.rotation.y = " + transform.rotation.y);
-        //    Debug.Log("vector3.y = " + vector3.y);
-        //    transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * _smooth);
-        //    yield return null;
-        //}
-
-        ////////////////////////////////////////////////////////
-
-        //  Quaternion target = Quaternion.Euler(rotationX, transform.rotation.y, rotationZ);
-        //for (; transform.rotation.x != _rotationX && transform.rotation.z != _rotationZ;)
-        //{
-        //Quaternion newRotation = transform.rotation;
-        //if (transform.rotation.x < _rotationX)
-        //{
-        //    newRotation.x += _smooth;
-        //}
-        //Debug.Log("+");
-        //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * _smooth);
-        //}
-
-
     }
 
     private void OnDie(EnemyHP enemyHP)
